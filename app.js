6e3ec076874a1829e8f37d4b736a7aa230ae185e
ArthurCorpO <arthur.corporativo@outlook.com>
2022-07-12 15:39:43 -0300

    updates for heroku7

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 
-const port = process.env.PORT || "8080";
+const port = process.env.PORT || "3000";
 const app = express();
 
 const cors = require("cors");
